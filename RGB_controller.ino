911664c13daf63246b5c8f4737a63db37f3ae90a
dstroppolo <stroppolo.daniel@gmail.com>
2017-12-26 03:58:58 -0500

    Add button and new modes
    - Added all the modes (different colors)
    - Added button functionality to cycle through the modes

--- RGB_controller.ino
+++ RGB_controller.ino
@@ -10,6 +10,7 @@
 int red = 11;           // the PWM pin the LED is attached to
 int green = 10;
 int blue = 6;
+int buttonPin = 8;
 
 int r = 0;
 int b = 0;
@@ -23,6 +24,11 @@ bool redInc = false;
 bool blueInc = true;
 bool greenInc = true;
 
+int buttonState;             // the current reading from the input pin
+int lastButtonState = HIGH;   // the previous reading from the input pin
+
+int currentMode = 0;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -30,17 +36,104 @@ void setup() {
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  r = analogRead(A0);
-  b = analogRead(A5);
-  g = analogRead(A1);
+
+  int buttonState = digitalRead(buttonPin);
   
-  if(redInc){
+  if(buttonState != lastButtonState && buttonState == HIGH){
+    currentMode++;
+    if(currentMode >= 8){
+      currentMode = 0;
+    }
+  }
+
+  switch (currentMode) {
+
+    case 0:
+      coal();
+      break;
+    case 1:
+      iron();
+      break;
+    case 2:
+      gold();
+      break;
+    case 3:
+      redstone();
+      break;
+    case 4:
+      azur();
+      break;
+    case 5:
+      emerald();
+      break;
+    case 6:
+      diamond();
+      break;
+    case 7:
+      fade();
+      break;
+    default:
+      diamond();
+  }
+  
+
+   lastButtonState = buttonState;
+
+  delay(20);
+
+}
+
+void coal(){
+  analogWrite(red,250);
+  analogWrite(blue,250);
+  analogWrite(green,250);
+}
+
+void iron(){
+  analogWrite(red,102);
+  analogWrite(blue,204);
+  analogWrite(green,153);
+}
+
+void redstone(){
+  analogWrite(red,0);
+  analogWrite(blue,255);
+  analogWrite(green,255);
+}
+
+void gold() {
+  analogWrite(red,0);
+  analogWrite(blue,255);
+  analogWrite(green,40);
+}
+
+void diamond() {
+  analogWrite(red,245);
+  analogWrite(blue,0);
+  analogWrite(green,0);
+}
+
+void emerald() {
+  analogWrite(red,255);
+  analogWrite(blue,240);
+  analogWrite(green,20);
+}
+
+void azur(){
+  analogWrite(red,255);
+  analogWrite(blue,0);
+  analogWrite(green,150);
+}
+
+void fade() {
+
+   if(redInc){
     redValue = incrementToMax(redValue);
     if(redValue >= 255){ redInc = false;}
   } else {
@@ -64,14 +157,13 @@ void loop() {
     if(greenValue <= 0){ greenInc = true;}
   }
 
- 
-
   analogWrite(red,redValue);
   analogWrite(blue,blueValue);
   analogWrite(green,greenValue);
 
-  delay(20);
+  delay(2);
 
+  
 }
 
 int incrementToMax(int value) {
